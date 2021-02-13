0f786f27880865735d7642f47a04fbe57d8e01af
BankNatchapol <natchapol.pat@gmail.com>
2021-02-13 14:54:29 +0700

    feat: space color
    add space color change
    
    Resolves: -
    See also: -

---
+++ spaces.py
@@ -0,0 +1,22 @@
+import cv2 as cv
+
+img = cv.imread('photos/nature.jpg')
+cv.imshow('Original', img)
+
+# BGR to Gray 
+gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+cv.imshow('Gray', gray)
+
+# BGR to HSV
+hsv = cv.cvtColor(img, cv.COLOR_BGR2HSV)
+cv.imshow('HSV', hsv)
+
+# BGR to LAB
+lab = cv.cvtColor(img, cv.COLOR_BGR2LAB)
+cv.imshow('LAB', lab)
+
+# BGR to RGB
+rgb = cv.cvtColor(img, cv.COLOR_BGR2RGB)
+cv.imshow('RGB', rgb)
+
+cv.waitKey(0)
