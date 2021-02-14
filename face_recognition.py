31eb748851e4ff05e9b1c46ae0695ee0f9db7d2b
BankNatchapol <natchapol.pat@gmail.com>
2021-02-14 11:25:33 +0700

    feat: face recognition
    add face recognition function
    
    Resolves: -
    See also: -

--- face_recognition.py
+++ face_recognition.py
@@ -7,3 +7,22 @@ people = ['lisa', 'rose', 'jennie', 'jisoo']
 
 face_recognizer = cv.face.LBPHFaceRecognizer_create()
 face_recognizer.read('recognizer/face_trained.yml')
+
+img = cv.imread('photos/blackpink/lisa_rose.jpg')
+cv.imshow('Target',img)
+
+gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+faces_rect = haar_cascade.detectMultiScale(gray, 1.1, 4)
+
+for (x,y,w,h) in faces_rect:
+    faces_roi = gray[y:y+h, x:x+h]
+
+    label, confidence = face_recognizer.predict(faces_roi)
+    print(f'Label = {people[label]} with a confidence of {confidence}')
+
+    cv.putText(img, str(people[label]), (x,y), cv.FONT_HERSHEY_COMPLEX, 1.0, (0,255,0), thickness=2)
+    cv.rectangle(img, (x,y), (x+w,y+h), (0,255,0), thickness=1)
+
+cv.imshow('Detected Faces', img)
+
+cv.waitKey(0)
